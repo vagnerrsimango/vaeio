3d7a61ed969002dff6c5bbba1411aa440a4238d9
unknown <vagnerrsimango@gmail.com>
2023-12-03 12:10:53 +0200

    eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import About from "@/components/About";
-import Footer from "@/components/Footer";
-import Intro from "@/components/Intro";
 import Hero from "@/components/Hero";
 import Guide from "@/components/Guide";
 import Portfolio from "@/components/Portfolio";
